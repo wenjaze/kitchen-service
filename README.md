b37096b94da6e502939cbb16e96b5df554eeb9b5
wenjaze <raska.gergely@gmail.com>
2022-09-16 16:50:11 +0200

    Add error handler

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kitchen-service
+Kitchen service is a service for specific domestical, culinar purposes. 
+The app also helps staying cost-effient, while still trying to have diverse cuisine.
--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,27 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const middleware = require('./middleware');
 
 const PORT = process.env.PORT || 1337;
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to /home");
-})
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:4200',
+}));
 
-app.post("/upload", (req, res) => {
-
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
 });
 
+app.use(middleware.notFound);
+app.use(middleware.errorHandler);
+
 app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`); // eslint-disable-line
+	console.log(`Listening at http://localhost:${PORT}`); // eslint-disable-line
 });
